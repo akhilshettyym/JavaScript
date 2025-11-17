12209b70337c6ec8e5ddb7ad9a258f72bacd9855
Shetty, Akhil <akhilshettym2003@gmail.com>
2025-11-17 19:20:19 +0530

    File folder structure with basics

---
+++ 01_BASICS/B_Syntax/A_Syntax.js
@@ -0,0 +1,77 @@
+// Syntax Rules : Are rules how programs must be constructed.
+
+// How to Declare variables:
+let x = 5;
+let y = 6;
+
+// How to Compute values:
+let z = x + y;
+
+// I am a Comment. I do Nothing
+
+/*
+JavaScript Values : 
+The JavaScript syntax defines two types of values:
+- Literals (Fixed values)
+- Variables (Variable values)
+
+* JavaScript Literals (Fixed values) :
+- The most important syntax rules for literals (fixed values) are:
+- Numbers are written with or without decimals: 10.50, 1001.
+- Strings are text, written within double or single quotes: "Shetty", 'Shetty'
+
+* JavaScript Keywords :
+- Are used to define actions which needs to be performed.
+- The let and const keywords create variables.
+let a = 10;
+const fname = "Shetty";
+
+NOTE : JS keywords are case-sensitive.
+JS does not interpret LET or Let as the keyword let.
+
+* Variables (Variable values) :
+- Variables are containers for storing data values.
+- Variables must be identified with unique names.
+
+Define x as a varible : let x;
+Assign the value 6 to x : x = 6;
+
+* JavaScript Identifiers :
+- An identifier is the name you give to a variable.
+Rules for identifiers:
+- Must start with a letter, _, or $
+- Can contain digits after the first character
+- Cannot be a reserved keyword (let, const, if, etc.)
+- Are case-sensitive
+
+* JavaScript Operators :
+- JavaScript assignment operators (=) assign values to variables:
+let x = 5;
+let y = 6;
+let sum = x + y;
+
+- JavaScript uses arithmetic operators ( + - * / ) to compute values: 5 * 10
+
+* JavaScript Expression :
+- An expression is a combination of values, variables, and operators, which computes to a value.
+- (5 + 6) * 10 evaluates to 110: (5 + 6) * 10
+
+- Expressions can also contain variable: x * 10
+- "John" + " " + "Doe", evaluates to "John Doe": "John" + " " + "Doe"
+
+* JavaScript is Case Sensitive :
+- JavaScript identifiers are case sensitive.
+The variables lastName and lastname, are different variables:
+Example
+let lastName = "Doe";
+let lastname = "Peterson";
+
+
+* JavaScript and Camel Case
+Hyphens: first-name, last-name, master-card, inter-city.
+Hyphens are not allowed in JavaScript. They are reserved for subtractions.
+- Underscore: first_name, last_name, master_card, inter_city.
+- Upper Camel Case (Pascal Case): FirstName, LastName, MasterCard, InterCity.
+- Lower Camel Case: firstName, lastName, masterCard, interCity.
+JavaScript programmers tend to use lower camel case.
+*/
