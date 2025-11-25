671943c3508a23f578aa1deb26c7614fb0f0281c
Shetty, Akhil <akhilshettym2003@gmail.com>
2025-11-25 16:36:00 +0530

    Event capturing

--- 02_ADVANCED/Events/Event_3/Mouse.js
+++ 02_ADVANCED/Events/Event_3/Mouse.js
@@ -1,5 +1,10 @@
 /*
-Event Bubbling : 
+
+Whenever a user performs a click or any event is raised . The event flow happens in two phases.
+1. Event will move from top level towards bottom level.
+2. Event raised element towards parent.
+
+* Event Bubbling : 
 Event bubbling in JavaScript is a mechanism where an event, triggered on a specific DOM element, 
 propagates upwards through its parent elements in the DOM hierarchy. When an event occurs on an 
 element (the target element), the event first triggers any attached event handlers on that element. 
@@ -7,11 +12,12 @@ Then, the event "bubbles up" to its immediate parent, triggering any handlers at
 and this process continues up the DOM tree until it reaches the document object. 
 Example : 
 div main
-    div nav - if nav has the event then this will be triggered if button is clicked.
-        links
-            button  - if button has no events, moves upwards insearch of an event
+div nav - if nav has the event then this will be triggered if button is clicked.
+links
+button  - if button has no events, moves upwards insearch of an event
 
 * Event capturing :
+Event travles from the root of the document tree down to the target element that initiated the event.
 
 */
 
