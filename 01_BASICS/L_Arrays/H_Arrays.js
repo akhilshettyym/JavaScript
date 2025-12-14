47764326c54c487f23212001ca543f30c9d42022
Shetty, Akhil <akhilshettym2003@gmail.com>
2025-12-14 21:21:00 +0530

    Formatted file

--- 01_BASICS/L_Arrays/H_Arrays.js
+++ 01_BASICS/L_Arrays/H_Arrays.js
@@ -30,48 +30,48 @@ arr.reverse();
 console.log("Reverse :", arr);
 
 let array = [11, 62, 3, 4, 25];
-array.sort(function(a, b){
-    return a-b;                 // a-b is for ascending order, b-a is for descending order.
+array.sort(function (a, b) {
+    return a - b;                 // a-b is for ascending order, b-a is for descending order.
 });
 console.log("Sort :", array);
 
 // forEach, map, reduce, filter
 let myArray = [10, 20, 30, 40, 50];
 
-myArray.forEach(function(val){
-    console.log("forEach", val+5); 
+myArray.forEach(function (val) {
+    console.log("forEach", val + 5);
 })
 
 // map is used when we have to create a new array on basis of older one.
 // When map is seen create a blank array.
-let newMyArray = myArray.map(function(val){
-    if(val > 10) return console.log(val);
+let newMyArray = myArray.map(function (val) {
+    if (val > 10) return console.log(val);
 })
 
 // Filter
-let newFilArray = myArray.filter(function(val){
-    if(val > 4) return true;
+let newFilArray = myArray.filter(function (val) {
+    if (val > 4) return true;
 })
 
 // Reduce
 let test = [1, 2, 3, 4, 5, 6];
-let ans = test.reduce(function(accumulator, val) {
+let ans = test.reduce(function (accumulator, val) {
     return accumulator + val;
 }, 0);
 
 // Find
-test.find(function(val){
+test.find(function (val) {
     return val === 1;
 })
 
 // Some
 let ar = [10, 30, 35, 90];
-let any = ar.some(function(val) {
+let any = ar.some(function (val) {
     return val > 85;
 })
 
 // Every
-let eve = ar.every(function(val) {
+let eve = ar.every(function (val) {
     return val > 5;
 })
 
