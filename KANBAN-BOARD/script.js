317ade02c97cb900efd13fd43dfc83d3a4a889b5
Shetty, Akhil <akhilshettym2003@gmail.com>
2025-12-27 19:27:17 +0530

    Kanban board Added task modal with active drags

--- KANBAN-BOARD/script.js
+++ KANBAN-BOARD/script.js
@@ -21,6 +21,8 @@ tasks.forEach(task => {
 //     progress.classList.remove("hover-over")
 // })
 
+const columns = [todo, progress, done];
+
 function addDrag(column) {
     column.addEventListener("dragenter", (e) => {
         e.preventDefault();
@@ -38,14 +40,74 @@ function addDrag(column) {
 
     column.addEventListener("drop", (e) => {
         e.preventDefault();
-        
+
         // console.log("Dropped", draggedElement, column);
 
         column.appendChild(draggedElement);
         column.classList.remove("hover-over");
+
+        // Count Logic
+        columns.forEach(col => {
+            const tasks = col.querySelectorAll(".task");
+            const count = col.querySelector(".right");
+
+            count.innerHTML = tasks.length;
+        })
     })
 }
 
 addDrag(todo);
 addDrag(progress);
 addDrag(done);
+
+// Modal active logic
+const toggleModalButton = document.querySelector("#toggle-modal");
+const modal = document.querySelector(".modal");
+
+toggleModalButton.addEventListener("click", () => {
+    modal.classList.toggle("active")
+})
+
+// Add Task Logic
+const addTaskBtn = document.querySelector("#add-new-task");
+addTaskBtn.addEventListener("click", () => {
+    const taskTitle = document.querySelector("#task-title-input").value;
+    const taskDesc = document.querySelector("#task-desc-input").value;
+
+    const div = document.createElement("div");
+
+    div.classList.add("task");
+    div.setAttribute("draggable", "true");
+
+    div.innerHTML = `
+    <h2>${taskTitle}</h2>
+    <p>${taskDesc}</p>
+    <button>Delete</delete>
+    `
+    todo.appendChild(div);
+
+    div.addEventListener("drag", (e) => {
+        draggedElement = div;
+    })
+
+    modal.classList.remove("active");
+
+    columns.forEach(col => {
+        const tasks = col.querySelectorAll(".task");
+        const count = col.querySelector(".right");
+
+        count.innerHTML = tasks.length;
+    })
+})
+
+
+
+// Modal close Logic
+const togCanBtn = document.querySelector("#cancel-task");
+const modalBg = document.querySelector(".modal .bg")
+togCanBtn.addEventListener("click", () => {
+    modal.classList.remove("active");
+})
+modalBg.addEventListener("click", () => {
+    modal.classList.remove("active");
+})
