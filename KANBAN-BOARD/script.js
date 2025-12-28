4ded30a56dd838329dc88726625a9a8cfca5461f
Shetty, Akhil <akhilshettym2003@gmail.com>
2025-12-28 22:10:30 +0530

    Kanban board functions

--- KANBAN-BOARD/script.js
+++ KANBAN-BOARD/script.js
@@ -1,3 +1,5 @@
+let tasksData = {};
+
 const todo = document.querySelector("#todo");
 const progress = document.querySelector("#progress");
 const done = document.querySelector("#done");
@@ -6,20 +8,68 @@ const tasks = document.querySelectorAll(".task");
 
 let draggedElement = null;
 
+function addTask(title, desc, column) {
+    const div = document.createElement("div");
+    div.classList.add("task");
+    div.setAttribute("draggable", "true");
+
+    div.innerHTML = `
+            <h2>${title}</h2>
+            <p>${desc}</p>
+            <button>Delete</button>
+            `
+    column.appendChild(div);
+
+    div.addEventListener("drag", (e) => {
+        draggedElement = div;
+    });
+
+    const deleteBtn = div.querySelector("button");
+    deleteBtn.addEventListener("click", () => {
+        div.remove();
+        updateTaskCount();
+    })
+    return div;
+}
+
+function updateTaskCount() {
+    columns.forEach(col => {
+        const tasks = col.querySelectorAll(".task");
+        const count = col.querySelector(".right");
+
+        // Storage Logic
+        tasksData[col.id] = Array.from(tasks).map(t => {
+            return {
+                title: t.querySelector("h2").innerText,
+                desc: t.querySelector("p").innerText
+            }
+        })
+        localStorage.setItem("tasks", JSON.stringify(tasksData));
+
+        count.innerHTML = tasks.length;
+    })
+}
+
+if (localStorage.getItem("tasks")) {
+    const data = JSON.parse(localStorage.getItem("tasks"));
+    console.log("DATA", data);
+    for (const col in data) {
+        const column = document.querySelector(`#${col}`);
+        data[col].forEach(task => {
+            addTask(task.title, task.desc, column);
+        })
+
+        const tasks = column.querySelectorAll(".task");
+        const count = column.querySelector(".right");
+        count.innerText = tasks.length;
+    }
+}
+
 tasks.forEach(task => {
     task.addEventListener("drag", (e) => {
-        // console.log("Dragging", e)
         draggedElement = task;
     })
 })
-
-// Progress
-// progress.addEventListener("dragenter", (e)=> {
-//     progress.classList.add("hover-over")
-// })
-// progress.addEventListener("dragleave", (e)=> {
-//     progress.classList.remove("hover-over")
-// })
 
 const columns = [todo, progress, done];
 
@@ -41,10 +91,10 @@ function addDrag(column) {
     column.addEventListener("drop", (e) => {
         e.preventDefault();
 
-        // console.log("Dropped", draggedElement, column);
-
         column.appendChild(draggedElement);
         column.classList.remove("hover-over");
+
+        updateTaskCount();
 
         // Count Logic
         columns.forEach(col => {
@@ -73,34 +123,15 @@ const addTaskBtn = document.querySelector("#add-new-task");
 addTaskBtn.addEventListener("click", () => {
     const taskTitle = document.querySelector("#task-title-input").value;
     const taskDesc = document.querySelector("#task-desc-input").value;
+    addTask(taskTitle, taskDesc, todo);
 
-    const div = document.createElement("div");
-
-    div.classList.add("task");
-    div.setAttribute("draggable", "true");
-
-    div.innerHTML = `
-    <h2>${taskTitle}</h2>
-    <p>${taskDesc}</p>
-    <button>Delete</delete>
-    `
-    todo.appendChild(div);
-
-    div.addEventListener("drag", (e) => {
-        draggedElement = div;
-    })
+    updateTaskCount();
 
     modal.classList.remove("active");
 
-    columns.forEach(col => {
-        const tasks = col.querySelectorAll(".task");
-        const count = col.querySelector(".right");
-
-        count.innerHTML = tasks.length;
-    })
+    document.querySelector("#task-title-input").value = "";
+    document.querySelector("#task-desc-input").value = "";
 })
-
-
 
 // Modal close Logic
 const togCanBtn = document.querySelector("#cancel-task");
